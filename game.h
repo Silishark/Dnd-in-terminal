bac76adaa408e6fb43c5d09a26ae4eea0b5344ff
mkitsdts <mkitsdts@outlook.com>
2024-09-04 18:53:28 +0800

    game logic

---
+++ game.h
@@ -0,0 +1,23 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <QWidget>
+
+namespace Ui
+{
+class Game;
+}
+
+class Game : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Game(QWidget *parent = nullptr);
+    ~Game();
+    void init();
+private:
+    Ui::Game *ui;
+};
+
+#endif // GAME_H
