f5fdd8e301aa131d8ddb12794b80624fd01c051f
Silishark <sharkhongyu@gmail.com>
2024-09-06 16:58:12 +0800

    Add files via upload

--- character.h
+++ character.h
@@ -1,26 +1,65 @@
-#ifndef CHARACTER_H
-#define CHARACTER_H
+#ifndef character_h
+#define character_h
+#include <QFile>
+#include <QTextStream>
+#include <QVector>
+#include <Qstring>
+#include "action/action.h"
 
-#include <QString>
-
-class Character
+class character
 {
 public:
-    Character(QString _name, QString _career): weapon(false), name(_name), career(_career)
+character(
+    Qstring name,int health,int attack,int defense):
+    cname(name),chealth(health),cattack(attack),cdefense(defense)
+    {loadmemory();}
+    virtual ~character()
+    {savememory();}
+    void addmemory(const Qstring& memory)
     {
+        cmemory=memory;
     }
-    ~Character() = default;
+    void loadmemory()
+      {
+        QFile
+        file(memoryFilename());
+        if(file.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            QTextStream in(&file);
+            cmemory=in.readAll();
+            file.close();
+        }
+        
+      }
+      void save memory()
+      {
+        QFile file(memoryFilename());
+        if(file.open(QIODevice::WriteOnly | QIODevice::Text))
+        {
+            QTextStream out(&file);
+            out<<cmemory;
+            file.close();
+        }
+      }
+      void addAction(Action* action) {
+        cactions.append(action);
+    }
 
-    virtual void init();
-    virtual int Cskill();
-
-    Character& operator=(Character const&) = delete;
-    Character(Character const&) = delete;
-
-    bool weapon;
-    QString name;
-    QString career;
-};
-
-#endif // CHARACTER_H
-
+    QString performAction(int index) {
+        if (index >= 0 && index < cactions.size()) {
+            return cactions[index]->perform();
+        }
+        return "Invalid action";
+    }
+      QString getmemory() const {return cmemory;}
+      protected:
+      QString memoryFilename() const{return Qstring("%1memory.txt").arg(cname);}
+      private:
+      QString cname;
+      int chealth;
+      int cattack;
+      int cdefense;
+      QString cmemory;
+      QVector<Action*> cactions;
+}
+#endif
